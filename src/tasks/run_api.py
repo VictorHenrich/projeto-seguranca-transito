1a60987a16c749b852a191ac1df8c28bc537c8c2
VictorHenrich <victorhenrich993@gmail.com>
2023-02-22 13:39:02 -0300

    changes

--- src/tasks/run_api.py
+++ src/tasks/run_api.py
@@ -5,6 +5,10 @@ from server.cli import Task
 @app.cli.add_task("api", "run", "r", "Inicializa a aplicação HTTP")
 class RunApi(Task):
     def execute(self) -> None:
-        import controllers.http
+        import controllers.http.autenticacao
+        import controllers.http.departamento
+        import controllers.http.ocorrencias
+        import controllers.http.usuarios
+        import controllers.http.usuarios_departamento
 
         app.http.run()
