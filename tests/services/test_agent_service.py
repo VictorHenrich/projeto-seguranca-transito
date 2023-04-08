06a57ccd6bdc2cdf043c7becfb0d47a90f6e5cc3
Victor Henrich <victor.henrich@globalcobranca.com.br>
2023-04-08 16:29:33 -0300

    Testado servico de agentes

--- tests/services/test_agent_service.py
+++ tests/services/test_agent_service.py
@@ -6,7 +6,7 @@ from ..util import TestUtil
 
 TestUtil.load_modules()
 
-
+import src.start.application
 from src.patterns.service import IService
 from src.models import Agent
 from src.services.agent import (
@@ -34,10 +34,10 @@ class TestAgentService(TestCase):
         departament.id = 1
 
         agent_creation_props.departament = departament
-        agent_creation_props.name = ""
-        agent_creation_props.access = ""
-        agent_creation_props.password = ""
-        agent_creation_props.position = ""
+        agent_creation_props.name = "Usuário teste"
+        agent_creation_props.access = "teste"
+        agent_creation_props.password = "1234"
+        agent_creation_props.position = "TESTE"
 
         agent_creation_service: IService[
             AgentCreationServiceProps, None
@@ -52,12 +52,12 @@ class TestAgentService(TestCase):
 
         departament.id = 1
 
-        agent_update_props.agent_uuid = ""
+        agent_update_props.agent_uuid = "8b19abfc-94b2-4494-92e9-ae5fcf79add6"
         agent_update_props.departament = departament
-        agent_update_props.name = ""
-        agent_update_props.access = ""
-        agent_update_props.password = ""
-        agent_update_props.position = ""
+        agent_update_props.name = "Nome Alterado"
+        agent_update_props.access = "teste"
+        agent_update_props.password = "1234"
+        agent_update_props.position = "funcionário publico"
 
         agent_update_service: IService[
             AgentUpdateServiceProps, None
@@ -72,7 +72,8 @@ class TestAgentService(TestCase):
 
         departament.id = 1
 
-        agent_exclusion_props.agent_uuid = ""
+        agent_exclusion_props.agent_uuid = "8b19abfc-94b2-4494-92e9-ae5fcf79add6"
+        agent_exclusion_props.departament = departament
 
         agent_exclusion_service: IService[
             AgentExclusionServiceProps, None
@@ -87,7 +88,7 @@ class TestAgentService(TestCase):
 
         departament.id = 1
 
-        agent_finding_props.agent_uuid = ""
+        agent_finding_props.agent_uuid = "d9f848e2-113b-4c2e-8407-a7cb41b364fa"
         agent_finding_props.departament = departament
 
         agent_finding_service: IService[
@@ -118,9 +119,9 @@ class TestAgentService(TestCase):
     def test_auth_agent(self) -> None:
         agent_auth_props: Mock = Mock()
 
-        agent_auth_props.departament_access = ""
-        agent_auth_props.user = ""
-        agent_auth_props.password = ""
+        agent_auth_props.departament_access = "T"
+        agent_auth_props.user = "victor.henrich"
+        agent_auth_props.password = "1234"
 
         agent_auth_service: IService[
             AgentAuthorizationServiceProps, str
