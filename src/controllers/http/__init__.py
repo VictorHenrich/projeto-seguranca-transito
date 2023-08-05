b9ff351d65b48599e27261734eb0157b5efb69e8
Victor Henrich <victor.henrich@globalcobranca.com.br>
2023-08-04 23:23:00 -0300

    changes

--- src/controllers/http/__init__.py
+++ src/controllers/http/__init__.py
@@ -6,4 +6,4 @@ from .occurrence_controller import (
 from .user_controller import UserRegisterController, UserQueryController
 from .user_auth_controller import UserAuthController
 from .attachment_controller import AttachmentQueryController
-from vehicle_controller import VehicleQueryController
+from .vehicle_controller import VehicleQueryController
